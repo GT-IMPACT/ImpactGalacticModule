40e927cc391fa17278db1f9624f6e5818abfda16
Lex <domian844@gmail.com>
2020-12-05 21:51:42 +0700

    change ID dimensions

--- src/main/java/com/impact/core/Config.java
+++ src/main/java/com/impact/core/Config.java
@@ -2,9 +2,15 @@ package com.impact.core;
 
 public class Config {
 
-    public static int dimensionIDMakeMake = -1010;
-    public static int dimensionIDHaumea = -2000;
-    public static int dimensionIDPhobos = -2001;
-    public static int dimensionIDDeimos = -2002;
+    /**
+     *   ID Dimensions GalaxySpace: 2000 - 2050
+     *   ID Dimensions Impact Galactic Module 2050 - 2100
+     */
+
     public static int dimensionIDEmpty = -1;
+    public static int dimensionIDMakeMake = -2050;
+    public static int dimensionIDHaumea = -2051;
+    public static int dimensionIDPhobos = -2052;
+    public static int dimensionIDDeimos = -2053;
+
 }
